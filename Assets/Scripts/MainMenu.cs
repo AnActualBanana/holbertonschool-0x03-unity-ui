836f653b9a46537ef1d720e9ecbb556b48ee10ee
AnActualBanana <loganmcclain59@gmail.com>
2023-02-17 12:48:10 -0600

    task 12

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,12 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+	public Material trapMat;
+	public Material goalMat;
+	public Toggle colorblindMode;
 	public void PlayMaze()
 	{
 		SceneManager.LoadScene("maze");
+		 if (colorblindMode != null && colorblindMode.isOn)
+    	{
+        trapMat.color = new Color32(255, 112, 0, 1);
+        goalMat.color = Color.blue;
+    	}
 	}
 
     public void QuitMaze()
